7c32dcd275fbbb627205a3e607063bd55216388a
le minh tuyen <leminhtuyen280906@gmail.com>
2025-10-15 15:57:09 +0700

    Initial commit

---
+++ Buoi1/Main.java
@@ -0,0 +1,17 @@
+package Java_Private.Buoi1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap giay: ");
+        int tgian = sc.nextInt();
+
+        int gio=tgian/3600;
+        int phut=(tgian%3600)/60;
+        int giay=(tgian%60);
+        System.out.println("  Gio: "+gio+"  Phut: "+phut+"   Giay: "+giay);
+
+    }
+}
