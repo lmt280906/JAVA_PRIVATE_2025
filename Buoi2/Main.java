efb4697bd33964db5e28cf7fb58e0acd0ccb53a7
le minh tuyen <leminhtuyen280906@gmail.com>
2025-10-25 16:32:02 +0700

    Bai tap tuan 2

---
+++ Buoi2/Main.java
@@ -0,0 +1,48 @@
+package Java_Private.Buoi2;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a[] = {2, 15, 6, 4, 13, 11, 16, 7, 3, 5, 17};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--MENU--");
+        System.out.println("1. Tính tổng các phẩn tử trong mảng");
+        System.out.println("2. In ra phần tử lớn nhất, nhỏ nhất trong mảng");
+        System.out.println("3. Sắp xếp lại mảng theo chiều tăng dần");
+        System.out.println("4. In ra số nguyên tố lớn nhất trong mảng (nếu không có in ra Không có)");
+        System.out.print("Hãy nhập lựa chọn : ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Tổng mảng  :" + Utils.tong(a));
+                break;
+            case 2:
+                System.out.println("Max là : " + Utils.max(a));
+                System.out.println("Min là : " + Utils.min(a));
+                break;
+            case 3:
+                Utils.tang_dan(a);
+                System.out.println("Mảng sắp xếp tăng dần là :");
+                for(int x : a){
+                    System.out.print(x+" ");
+                }
+                break;
+            case 4:
+                int maxSNT = -1;
+                for (int x : a) {
+                    if (Utils.SNT(x)) {
+                        if (x > maxSNT) {
+                            maxSNT = x;
+                        }
+                    }
+                }
+                if (maxSNT != -1) {
+                    System.out.println("Số nguyên tố lớn nhất là : " + maxSNT);
+                } else
+                    System.out.println("Không có");
+                break;
+        }
+    }
+}
---
+++ Buoi2/Utils.java
@@ -0,0 +1,51 @@
+package Java_Private.Buoi2;
+
+public class Utils {
+    public static int tong(int a[]) {
+        int tong = 0;
+        for (int i = 0; i < a.length; i++) {
+            tong += a[i];
+        }
+        return tong;
+    }
+
+    public static int min(int a[]) {
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+        return min;
+    }
+
+    public static int max(int a[]) {
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void tang_dan(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static boolean SNT(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n % i == 0){
+                return false;
+            }
+        }return true;
+    }
+
+}
